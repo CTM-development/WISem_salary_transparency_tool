3c012f804ecd02151bb90e16afca275b05329ebd
Laptop <christopher@mminderlein.de>
2023-07-13 12:11:05 +0200

    Basic routing setup done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// components
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { SalaryOverviewComponent } from './salary-overview/salary-overview.component';
+import { SkillTreeComponent } from './skill-tree/skill-tree.component';
+
+const routes: Routes = [
+	{ 
+		path: 'landing-page', 
+		title: 'Salary Overview Tool - Landing Page', 
+		component: LandingPageComponent },
+	{ 
+		path: 'salary-overview',
+		title: 'Salary Overview Tool - Salary Overview',
+		component: SalaryOverviewComponent },
+	{ 
+		path: 'skill-tree',
+		title: 'Salary Overview Tool - Skill Tree',
+		component: SkillTreeComponent },
+
+	{ path: '', redirectTo: '/landing-page', pathMatch: 'full' },
+
+	{ path: '**', component: LandingPageComponent } // wildcard route -> matches any URL
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// angular material 
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
@@ -20,7 +25,11 @@ import { SalaryOverviewComponent } from './salary-overview/salary-overview.compo
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+		MatToolbarModule,
+		MatIconModule,
+		MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
