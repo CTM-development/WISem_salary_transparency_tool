9ad3d4219d3f894ba240bd3df37dfd8676a98014
ju92buje <lukas.willer@fau.de>
2023-07-23 16:23:39 +0200

    extended db but error on employee table: not data

--- src/app/skill-tree/skill-tree.component.ts
+++ src/app/skill-tree/skill-tree.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { liveQuery } from 'dexie';
+import { db, Role, Employee } from '../data/db';
+
 @Component({
   selector: 'app-skill-tree',
   templateUrl: './skill-tree.component.html',
@@ -7,4 +10,27 @@ import { Component } from '@angular/core';
 })
 export class SkillTreeComponent {
 
+  employee_name = "Peter Bla";
+  employee_role = "Test Engineer";
+
+  roles$ = liveQuery(() => db.roles.toArray());
+
+  identifyList(index: number, role: Role) {
+    return `${role.id}${role.name}`;
+  }
+  employee$ = liveQuery(() => db.employees.toArray());
+
+  identifyEmployee(index: number, emp: Employee) {
+    if (index == 0) {
+      console.log("Bla");
+      return `${emp.id}${emp.name}`;
+    }
+
+    return "err";
+  }
+
 }
+
+
+
+
