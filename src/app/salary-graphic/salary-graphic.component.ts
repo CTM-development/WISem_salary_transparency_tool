e80a9e393d9ba46fa79376541021f6324f00bd25
Laptop <christopher@mminderlein.de>
2023-07-25 18:01:51 +0200

    salary tower done

--- src/app/salary-graphic/salary-graphic.component.ts
+++ src/app/salary-graphic/salary-graphic.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
 @Component({
-  selector: 'app-salary-graphic',
-  templateUrl: './salary-graphic.component.html',
-  styleUrls: ['./salary-graphic.component.scss']
+	selector: 'app-salary-graphic',
+	templateUrl: './salary-graphic.component.html',
+	styleUrls: ['./salary-graphic.component.scss']
 })
 export class SalaryGraphicComponent {
-  
+	constructor() {
+
+	}
+
+	// scroll(target: string) {
+	// 	this.scrollService.scrollTo(target, 700, 0);
+	// 	console.log(target)
+	// }
 }
